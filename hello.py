8a3fea5a53e8dbc85f6b9514d50f47ab4208e840
Zuzana Kašáková <zuza.kasakova@gmail.com>
2017-02-28 22:11:11 +0100

    Cycle

--- hello.py
+++ hello.py
@@ -4,5 +4,5 @@ print("Start of the program")
 print("Hello there!")
 
 name = input("What's your name:")
-
-print("Hi {0}".format(name))
+for i in range(3):
+    print("Hi {0} for the {1}.time".format(name,i+1))
